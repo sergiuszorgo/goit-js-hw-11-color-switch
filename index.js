0f70e11c1c4bee0ce86d84e520181659cd247f2e
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-12-16 22:36:58 +0200

    Вроде работает

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
 const body = document.body;
 const startBtnRef = document.querySelector('[data-action="start"]');
 const stopBtnRef = document.querySelector('[data-action="stop"]');
@@ -5,10 +14,25 @@ const stopBtnRef = document.querySelector('[data-action="stop"]');
 startBtnRef.addEventListener('click', startСhangingСolor)
 stopBtnRef.addEventListener('click', stoptСhangingСolor)
 
+let interval = 0;
+
 function startСhangingСolor() {
+    startBtnRef.disabled = true;
+    interval = setInterval(painter, 1000);
     console.log('start');
 };
 
 function stoptСhangingСolor() {
+    startBtnRef.disabled = false;
     console.log('stop');
+    clearInterval(interval);
 };
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function painter() {
+    body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    console.log(body.style.backgroundColor);
+}
